6b8f1d2c8c6ba2a0d64e096f471f422c2ea76a0c
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2025-09-28 15:41:27 +0200

    Added Job and Employee models, updated jobs.html template to include jobs

--- ems/models.py
+++ ems/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Job(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    date = models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = PhoneNumberField(unique=True, region="US")
+    joining_date = models.DateField(default=timezone.now)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- ems/views.py
+++ ems/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def dash(request):
     return render(request, "dash.html")
 
 def jobs(request):
-    return render(request, "jobs.html")
+    jobs_data = Job.objects.all().order_by("date")
+    data = {"jobs": jobs_data}
+    return render(request, "jobs.html", data)
 
 def employees(request):
     return render(request, "employees.html")
